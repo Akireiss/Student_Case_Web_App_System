8ce9fd74f6450c73f32ca04659f007aa4e18a421
Akireiss <joshcstnd@gmail.com>
2023-06-26 16:09:32 +0800

    Guard and Auth

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
-use App\Http\Controllers\Admin\StudentController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\ReportController;
+use App\Http\Controllers\Admin\StudentController;
 use App\Http\Controllers\Admin\EmployeeController;
 use App\Http\Controllers\Admin\OffensesController;
 use App\Http\Controllers\Admin\ClassroomController;
+use App\Http\Controllers\Adviser\DashboardController;
+use App\Http\Controllers\Adviser\AdvisorDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,66 +37,77 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 
+Route::prefix('admin')->middleware(['auth', 'role'])->group(function () {
 
-Route::prefix('admin/settings')->middleware(['auth'])->group(function () {
-    //offenses
-    Route::get('offenses', [OffensesController::class, 'index']);
-    Route::get('offenses/create', [OffensesController::class, 'create']);
-    Route::post('offenses/store', [OffensesController::class, 'store']);
-    Route::get('offenses/{offense}/edit', [OffensesController::class, 'edit'])->name('offenses.edit');
-    Route::put('offenses/{id}', [OffensesController::class, 'update'])->name('admin.settings.offenses.update');
+    /*Admin Access*/
+    Route::middleware(['can:admin-access'])->group(function () {
+        Route::prefix('settings')->group(function () {
 
-    //employeee
-    Route::get('teachers', [EmployeeController::class, 'index']);
-    Route::get('teacher/create', [EmployeeController::class, 'create']);
-    Route::post('employee/store', [EmployeeController::class, 'store']);
+            /*Settings Area*/
+            /*offenses*/
+            Route::get('offenses', [OffensesController::class, 'index']);
+            Route::get('offenses/create', [OffensesController::class, 'create']);
+            Route::post('offenses/store', [OffensesController::class, 'store']);
+            Route::get('offenses/{offense}/edit', [OffensesController::class, 'edit'])->name('offenses.edit');
+            Route::put('offenses/{id}', [OffensesController::class, 'update'])->name('admin.settings.offenses.update');
 
+            /*EMPLOYEE*/
+            Route::get('teachers', [EmployeeController::class, 'index']);
+            Route::get('teacher/create', [EmployeeController::class, 'create']);
+            Route::post('employee/store', [EmployeeController::class, 'store']);
+            Route::get('employees/{employee}/edit', [EmployeeController::class, 'edit'])->name('employee.edit');
 
-
-    //classrooms
-    Route::get('classrooms', [ClassroomController::class, 'index']);
-    Route::get('classroom/create', [ClassroomController::class, 'create']);
-    Route::post('classroom/store', [ClassroomController::class, 'store']);
-    Route::get('classrooms/{id}/edit', [ClassroomController::class, 'edit']);
-    Route::put('classrooms/{classroom}', [ClassroomController::class, 'update'])->name('classrooms.update');
-
-
-    //Employee
-    Route::get('employees/{employee}/edit', [EmployeeController::class, 'edit'])->name('employee.edit');
-
-
-
-    Route::get('admin/student-profile', function () { return view('admin.student-profile.index');});
+            /*CLASSROOMS*/
+            Route::get('classrooms', [ClassroomController::class, 'index']);
+            Route::get('classroom/create', [ClassroomController::class, 'create']);
+            Route::post('classroom/store', [ClassroomController::class, 'store']);
+            Route::get('classrooms/{id}/edit', [ClassroomController::class, 'edit']);
+            Route::put('classrooms/{classroom}', [ClassroomController::class, 'update'])->name('classrooms.update');
 
 
+            /* Temporary as the backend is still in development*/
+            Route::get('audit-trail', function () {
+                return view('admin.settings.audit-trail.index');
+            });
 
-    // Temporary as the backend is still in development
-    Route::get('audit-trail', function () { return view('admin.settings.audit-trail.index');});
+            /* Students Area*/
+            Route::get('students', [StudentController::class, 'index']);
+            Route::get('students/create', [StudentController::class, 'create']);
+            Route::post('students/store', [ClassroomController::class, 'store']);
 
-    // Students Area
-    Route::get('students',[StudentController::class, 'index']);
-    Route::get('students/create',[StudentController::class, 'create']);
-    Route::post('students/store', [ClassroomController::class, 'store']);
-
-
-
-
+        });
+    });
 
 });
 
+/*Adviser | Staff Area */
+
+Route::prefix('adviser')->middleware(['auth', 'role'])->group(function () {
+    Route::middleware(['can:adviser-access'])->group(function () {
+        Route::get('dashboard', [AdvisorDashboardController::class, 'index']);
+    });
+});
+
+
+
+
+
 
 //Still neeed fix for this area
-
-
-Route::prefix('admin')->middleware(['auth'])->group(function (){
+Route::prefix('admin')->middleware(['auth'])->group(function () {
     Route::get('dashboard', function () {
         return view('admin.dashboard.dashboard');
     });
 
-    });
+});
+
+Route::get('admin/student-profile', function () {
+    return view('admin.student-profile.index');
+});
 
 
 
-    Route::get('admin/reports',[ReportController::class, 'index']);
-    Route::get('admin/update-acc',[UserController::class, 'update_acc']);
-    Route::get('admin/add-acc',[UserController::class, 'index']);
+
+Route::get('admin/reports', [ReportController::class, 'index']);
+Route::get('admin/update-acc', [UserController::class, 'update_acc']);
+Route::get('admin/add-acc', [UserController::class, 'index']);
